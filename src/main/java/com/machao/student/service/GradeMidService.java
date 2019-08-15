0df2ba46b3cca7734afa3819def6d9d46cc3f8c4
machao <248402636@qq.com>
2019-08-16 07:58:24 +0800

    add

--- src/main/java/com/machao/student/service/GradeMidService.java
+++ src/main/java/com/machao/student/service/GradeMidService.java
@@ -3,6 +3,6 @@ package com.machao.student.service;
 
 import com.machao.student.entity.GradeMid;
 
-public interface GradeMidSer {
+public interface GradeMidService {
     GradeMid selectByPrimaryKey(String name);
 }
