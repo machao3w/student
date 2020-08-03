78efd34b195987c432ae627bccc660822daaf594
machao3w <248402636@qq.com>
2020-08-03 13:33:47 +0800

    test

--- src/main/java/com/machao/student/configure/SecurityConfig.java
+++ src/main/java/com/machao/student/configure/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        //test
         http.addFilterBefore(validCodeFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin()
                 .loginPage("/login")
