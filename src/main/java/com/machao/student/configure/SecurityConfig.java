15385e34c3faf20dd5b792a53dbe6503e2fd045e
machao3w <248402636@qq.com>
2020-08-03 13:32:39 +0800

    test

--- src/main/java/com/machao/student/configure/SecurityConfig.java
+++ src/main/java/com/machao/student/configure/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+        public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
